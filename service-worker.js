501ba9cb0dcdf06fb2514e952bc5ed87c90b5b9a
basiliopedro <basiliopedropepe@gmail.com>
2026-02-24 12:14:55 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
 
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("connecthub").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./css/style.css"
+      ]);
+    })
+  );
+});
