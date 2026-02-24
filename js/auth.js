8522d749d1b7038df642db4b69b792744c15be2a
basiliopedro <basiliopedropepe@gmail.com>
2026-02-24 12:59:00 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,10 @@
-function register() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  auth.createUserWithEmailAndPassword(email, password)
-  .then(() => {
-    alert("Conta criada!");
-    window.location.href = "index.html";
-  })
-  .catch(error => alert(error.message));
-}
-
 function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   auth.signInWithEmailAndPassword(email, password)
   .then(() => {
-    alert("Login realizado!");
+    alert("Login realizado com sucesso!");
     window.location.href = "index.html";
   })
   .catch(error => alert(error.message));
