228c83c8c664e65847cf14e2f9aab755e963285b
basiliopedro <basiliopedropepe@gmail.com>
2026-02-24 12:10:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+const container = document.getElementById("professionals");
 
+db.collection("professionals").where("approved", "==", true)
+.onSnapshot(snapshot => {
+  container.innerHTML = "";
+  snapshot.forEach(doc => {
+    const data = doc.data();
+
+    container.innerHTML += `
+      <div class="card">
+        <h3>${data.name}</h3>
+        <p>${data.profession}</p>
+        <p>${data.location}</p>
+        <p class="star">⭐ ${data.rating || 0}</p>
+      </div>
+    `;
+  });
+});
